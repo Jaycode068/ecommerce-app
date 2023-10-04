b7faf2403ca31c6d4605fc169d707e489500444f
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2023-10-04 16:15:48 +0100

    created restful enpoint for Category and Product

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,32 +1,31 @@
 from flask import Flask
-
 from config import Config
 from app.extension import db
+from app.main import bp as main_bp
+from app.api.category import category_bp
+from app.api.product import product_bp
+from app.api import bp as user_bp
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://johnson:"Ibelieve1!"@localhost:3306/appdb'
 
     # Initialize Flask extensions here
     db.init_app(app)
 
     # Register blueprints here
-    from app.main import bp as main_bp
     app.register_blueprint(main_bp)
-    
-    #User endpoint blueprint
-    from app.api import bp as user_bp
     app.register_blueprint(user_bp)
+    app.register_blueprint(category_bp, url_prefix='/api')
+    app.register_blueprint(product_bp, url_prefix='/api')
 
-    # from app.posts import bp as posts_bp
-    # app.register_blueprint(posts_bp, url_prefix='/posts')
-
-    # from app.questions import bp as questions_bp
-    # app.register_blueprint(questions_bp, url_prefix='/questions')
+    
 
     @app.route('/test/')
     def test_page():
         return '<h1>Testing the Flask Application Factory Pattern</h1>'
 
     return app
+ 

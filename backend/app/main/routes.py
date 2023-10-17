6f3f8c45816327921da1d4578b9f0b9dc3d585d0
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2023-10-17 20:04:32 +0100

    change static files linking

--- backend/app/main/routes.py
+++ backend/app/main/routes.py
@@ -1,6 +1,15 @@
 from app.main import bp
+from flask import render_template
 
 
 @bp.route('/')
 def index():
-    return 'This is The Main Blueprint'
+    return render_template('shopzo.html')
+
+@bp.route('/users')
+def user():
+    return  render_template('user.html')
+
+@bp.route('/address')
+def add_address():
+    return  render_template('address.html')

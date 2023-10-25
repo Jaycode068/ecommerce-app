00fffa128baed3d3b143c967a8d403e754fccf38
Emmanuel Ugbem <ugbememmanuel4@gmail.com>
2023-10-25 11:38:37 +0100

    added relationship  between user and cart

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -8,12 +8,10 @@ class User(db.Model):
     password = db.Column(db.String(255), nullable=False)
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
-    
     confirmpasswd = ''
     addresses = db.relationship('Address', back_populates='user', cascade='all, delete-orphan')
     orders = db.relationship('Order', back_populates='user', cascade='all, delete-orphan')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
     
     def _init_(self, username, email):
         self.username = username
